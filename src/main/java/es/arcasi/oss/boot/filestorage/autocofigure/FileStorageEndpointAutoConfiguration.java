f8d9848286351a50df1d65ef9b642b83e25fd625
plopez <plopez@crcit.es>
2016-11-12 16:02:42 +0100

    Set conditional on missing bean annotation

--- src/main/java/es/arcasi/oss/boot/filestorage/autocofigure/FileStorageEndpointAutoConfiguration.java
+++ src/main/java/es/arcasi/oss/boot/filestorage/autocofigure/FileStorageEndpointAutoConfiguration.java
@@ -3,6 +3,7 @@ package es.arcasi.oss.boot.filestorage.autocofigure;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +13,9 @@ import es.arcasi.oss.boot.filestorage.web.rest.FileStorageController;
 import es.arcasi.oss.filestorage.services.FileStorageService;
 
 @Configuration
-@ConditionalOnWebApplication
 @AutoConfigureAfter(FileStorageAutoConfiguration.class)
+@ConditionalOnWebApplication
+@ConditionalOnMissingBean(value = FileStorageController.class)
 @ConditionalOnProperty(prefix = "arcasi.filestorage", name = { "endpoint-url" }, havingValue = "")
 public class FileStorageEndpointAutoConfiguration {
 
